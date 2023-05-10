e79837d006815aaea5dec677c2cdb7cd10045a08
Axel Daniel <axeelleguero@gmail.com>
2023-05-10 14:11:39 -0300

    Test incluir garaje en agencia

--- SoftwareConcesionaria/test/ar/edu/unlam/pb/TestAgencia.java
+++ SoftwareConcesionaria/test/ar/edu/unlam/pb/TestAgencia.java
@@ -2,13 +2,27 @@ package ar.edu.unlam.pb;
 
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+
 import org.junit.Test;
 
 public class TestAgencia {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void queSePuedaIncluirUnGarajeAUnaAgencia() {
+		String razonSocial = "SA";
+		Integer cuit = 19919919;
+		HashSet <Auto> autosDisponibles= new HashSet<>();
+		HashSet <Garaje> garajesDisponibles = new HashSet<>();
+		
+		Integer id = 000001;
+		String direccion = "CALLE FALSA 123";
+		Integer espaciosDisponibles = 100;
+		
+		Agencia agencia = new Agencia(razonSocial, cuit, garajesDisponibles);
+		Garaje garaje = new Garaje(id, direccion, espaciosDisponibles, autosDisponibles);
+		agencia.agregarGaraje(garaje);
+		assertNotNull(agencia.getGarajesDisponibles());
 	}
 
 }
